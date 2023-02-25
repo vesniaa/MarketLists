62b8cf3c3828815a3e184fa9b9dad09eac8f881b
vesniaa <pyssiki@yandex.ru>
2023-02-25 14:18:52 +0300

    Initial Commit

---
+++ MarketLists/MarketListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketListsApp.swift
+//  MarketLists
+//
+//  Created by Евгения Аникина on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MarketListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
